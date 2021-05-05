9a1a55d41545a57b81bf84e8e892d7774d38a9f3
CDNDragoon <tjgossage@gmail.com>
2021-05-05 22:25:54 +0000

    added exit

--- play.js
+++ play.js
@@ -5,3 +5,25 @@ const {connect} = require('./client')
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    console.log("exited");
+    process.exit()
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (data) => {
+    handleUserInput(data)})
+  return stdin;
+};
+setupInput();
+
+module.exports = {setupInput};
