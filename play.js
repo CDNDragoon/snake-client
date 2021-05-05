869b3397154aa811239a4994a1975da6b4d1d624
CDNDragoon <tjgossage@gmail.com>
2021-05-05 22:59:16 +0000

    import + export

--- play.js
+++ play.js
@@ -1,29 +1,7 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const {connect} = require('./client')
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const handleUserInput = function (data) {
-  if (data === "\u0003") {
-    console.log("exited");
-    process.exit()
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", (data) => {
-    handleUserInput(data)})
-  return stdin;
-};
 setupInput();
-
-module.exports = {setupInput};
