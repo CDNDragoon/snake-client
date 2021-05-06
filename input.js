d656141b362f929ed85591ec37d3391e3b95fd07
CDNDragoon <tjgossage@gmail.com>
2021-05-06 00:11:54 +0000

    added wasd

--- input.js
+++ input.js
@@ -1,12 +1,23 @@
+let connection;
+
 const handleUserInput = function (data) {
   if (data === "\u0003") {
     console.log("exited");
     process.exit()
+  } else if (data === "w") {
+    connection.write("Move: up"); 
+  } else if (data === "a") {
+    connection.write("Move: left");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  } else if (data === "d") {
+    connection.write("Move: right")
   }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
